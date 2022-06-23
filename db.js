07aac88842f805bd5f1add4cace5f4fee10cb0db
ahmad jabak <ahmadjabak5@gmail.com>
2022-06-23 22:56:06 +0300

    schema

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
+const { application } = require("express");
 const mongoose  = require("mongoose");
-
+const sche = require("./schema")
 module.exports = async ()=> {
     try{
-        await mongoose.connect(process.env.DB);
+        await mongoose.connect(process.env.DB,sche);
         console.log("Connected to Database Successfuly");
     }catch(error){
         console.log("Error in connecting to Database!");
