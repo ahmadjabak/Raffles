022dbe8e6ead850dfece10e81c9aae43ada11b27
ahmadmohammadjabak <ahmadjabak5@gmail.com>
2022-06-27 15:27:23 +0300

    homepage cards, contact us form and contact us design

--- raffle-cards/src/components/ContactUs/ContactUsForm.js
+++ raffle-cards/src/components/ContactUs/ContactUsForm.js
@@ -1,13 +1,18 @@
 import "./contact.css"
 import Footer from "../Footer/Footer"
+import ContactDesgin from "./ContactUsPageDesign"
 export default function ContactUs() {
   return (
     <div>
 
       <Footer />
+    
+      <ContactDesgin/>
+      
       <div class="card">
         <h2>Contact Us</h2>
         <div class="row">
+          <div class="frist">
           <div class="col">
             <div class="form-group">
               <label>First Name</label>
@@ -21,30 +26,37 @@ export default function ContactUs() {
               <input type="text" />
             </div>
           </div>
-
+          </div>
+          <div class="second">
           <div class="col">
             <div class="form-group">
               <label>Email</label>
               <input type="email" />
             </div>
           </div>
-
+          
           <div class="col">
             <div class="form-group">
               <label>Phone Number</label>
               <input type="text" />
             </div>
           </div>
-
+          </div>
+          <div class="third">
           <div class="col">
             <div class="form-group">
               <label>Message</label>
-              <textarea></textarea>
+              <input type="text" />
             </div>
           </div>
-
+          </div>
+          <div class="forth">
+          
           <div class="col">
+          <div class="form-group">
             <input type="submit" value="Submit" />
+            </div>
+          </div>
           </div>
         </div>
       </div>
---
+++ raffle-cards/src/components/ContactUs/ContactUsPageDesign.js
@@ -0,0 +1,21 @@
+
+import "./contactDesign.scss"
+export default function ContactDesgin() {
+    return (
+        <div class="all">
+        <div class="frame">
+            <div class="circle"></div>
+            <div class="line left"></div>
+            <div class="line right"></div>
+            <div class="bracket left"></div>
+            <div class="bracket right"></div>
+            <div class="small top"><span class="first-letter">S</span>tart</div>
+            <div class="big">Contacting</div>
+            <div class="small bottom">Us <span class="first-letter">N</span>ow</div>
+            <div class="hide top"></div>
+            <div class="hide bottom"></div>
+        </div>
+        </div>
+
+    )
+}
